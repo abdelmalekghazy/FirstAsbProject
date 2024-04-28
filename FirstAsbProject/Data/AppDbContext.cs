fa2e98569c0f7d18def8f43777992a133d1a69bb
engabdulmelik <eng.abdulmelik.gazi@gmail.com>
2024-04-28 11:36:31 +0300

    Add project files.

---
+++ FirstAsbProject/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FirstAsbProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAsbProject.Data
+{
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Item> Items { get; set; }
+    }
+}
